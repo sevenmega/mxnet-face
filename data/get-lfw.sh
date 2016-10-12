083cd23ddca42b52cce20847e6ffff406c577d9d
Wang Lei <lei.wang@bitmain.com>
2016-10-12 18:25:12 +0800

    add get-lfw script

---
+++ data/get-lfw.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+#
+# Download data, this script refer to https://github.com/tornadomeet/mxnet-face 
+
+printf "\n\n====================================================\n"
+printf "Downloading aligned lfw dataset here\n"
+printf "http://pan.baidu.com/s/1qYDxeRq\n"
+printf "with password : mg2i\n"
+printf "decompress to dir data/lfw-align\n"
+printf "====================================================\n\n"
