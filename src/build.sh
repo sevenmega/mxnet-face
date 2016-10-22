c2ee3de12f4b2cc5497917bb1a03fe10b3cb5f8c
Wang Lei <lei.wang@bitmain.com>
2016-10-22 13:11:59 +0800

    read face db from msgpack file

--- src/build.sh
+++ src/build.sh
@@ -4,6 +4,7 @@ gcc -o cam cam.cpp -std=c++11 \
     -I../../dlib -L../../dlib/dlib/build -ldlib \
     -I../../mxnet/include -L../../mxnet/amalgamation -lmxnet_predict \
     -I../../ccv/lib -L../../ccv/lib -lccv \
+    -I../../msgpack-c/include -L../../msgpack-c/build -lmsgpackc \
     -I/opt/OpenBLAS/include -L/opt/OpenBLAS/lib -lopenblas \
     -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_objdetect \
     -lpng -ljpeg \
